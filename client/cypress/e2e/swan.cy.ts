b8332d23055fa9fb6cc7824bb5ca2c6f4ef5ed45
Matthew Kelsey <matthewkelsey1985@gmail.com>
2023-04-10 17:30:50 +0100

    basic e2e tests

---
+++ client/cypress/e2e/swan.cy.ts
@@ -0,0 +1,26 @@
+describe("Login", () => {
+  it("logs in successfully", () => {
+    cy.visit("http://localhost:3000/login");
+
+    cy.get('input[name="email"]').type("new-User@user.com");
+    cy.get('input[name="password"]').type("newUser");
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should("include", "/home");
+    cy.contains("Upcoming Races");
+  });
+});
+
+describe("Odds", () => {
+  it("logs in and moves succesfully to odds page", () => {
+    cy.visit("http://localhost:3000/login");
+
+    cy.get('input[name="email"]').type("new-User@user.com");
+    cy.get('input[name="password"]').type("newUser");
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.race-div').first().click()
+    cy.url().should("include", '/odds')
+    cy.get('.horse-div').should('exist')
+  });
+});
